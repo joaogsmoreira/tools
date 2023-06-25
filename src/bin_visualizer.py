5bdccec8e4594c47b28f4a00778c2bc82122eb22
joaogsmoreira <1150672@isep.ipp.pt>
2023-06-25 15:51:04 +0100

    checkpoint

--- src/bin_visualizer.py
+++ src/bin_visualizer.py
@@ -15,9 +15,9 @@ def show_point_cloud(pc_data):
 
 
 if __name__ == '__main__':
-    #path_to_point_cloud = '/home/johny/CustomDataset/training/velodyne/000000.bin'
+    path_to_point_cloud = '/home/johny/CustomDatasetV2/training/velodyne/000000.bin'
     #path_to_point_cloud = '/home/johny/Kitti/gt_database/0_Pedestrian_0.bin'
-    path_to_point_cloud = '/home/johny/CustomDataset/gt_database/0_Pedestrian_0.bin'
+    #path_to_point_cloud = '/home/johny/CustomDatasetV2/gt_database/0_Pedestrian_0.bin'
 
     point_cloud_data = np.fromfile(path_to_point_cloud, '<f4')  # little-endian float32
     point_cloud_data = np.reshape(point_cloud_data, (-1, 4))  # x, y, z, r
